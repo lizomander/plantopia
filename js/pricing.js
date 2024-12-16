4238ee59e618f0cc814db156c69de42305c978cf
lizomander <131398608+lizomander@users.noreply.github.com>
2024-12-16 08:24:49 +0000

    xtra fix

--- js/pricing.js
+++ js/pricing.js
@@ -25,33 +25,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
-
-
-
-
-
-// function getTotalPrice(priceWOTax) {
-//     const taxRate = 0.19;
-//     return priceWOTax + priceWOTax * taxRate;
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const priceWithoutTaxInput = document.getElementById("priceWithoutTax");
-//     const calculatePriceBtn = document.getElementById("calculatePriceBtn");
-//     const priceWOTaxDisplay = document.getElementById("priceWOTaxDisplay");
-//     const priceWithTaxDisplay = document.getElementById("priceWithTaxDisplay");
-//     const convertButton = document.getElementById("convertCurrencyBtn");
-//     const convertedPriceDisplay = document.getElementById("convertedPrice");
-//     calculatePriceBtn.addEventListener("click", () => {
-//         const priceWOTax = parseFloat(priceWithoutTaxInput.value);
-//         if (isNaN(priceWOTax) || priceWOTax < 0) {
-//             alert("Please enter a valid positive number for the price.");
-//             return;
-//         }
-//         const priceWithTax = getTotalPrice(priceWOTax);
-
-//         priceWOTaxDisplay.textContent = priceWOTax.toFixed(2);
-//         priceWithTaxDisplay.textContent = priceWithTax.toFixed(2);
-//     });
-
-// });
